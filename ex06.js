0a67bead75818300bb3791ec537f4991a716b41d
Tankran007 <s6406021622150@email.kmutnb.ac.th>
2023-07-11 19:28:34 +0700

    update

--- ex06.js
+++ ex06.js
@@ -5,3 +5,6 @@ function findPerimeter(num1, num2){
 console.log(findPerimeter(6, 7));
 console.log(findPerimeter(20, 10));
 console.log(findPerimeter(2, 9));
+
+//ข้อนี้หาเส้นรอบรูป4เหลี่ยม เราก็เหลี่ยมจัด เส้น4เหลี่ยมมี4เส้น แบ่งเป็นเส้นที่เท่ากัน2เส้น 
+//ก็เลยเอาเส้นที่1*2+เส้นที่2*2= ความยาวของรูปสี่เหลี่ยม
