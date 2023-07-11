d7533bdc8b06ca21681f877f2d084899446d862d
Tankran007 <s6406021622150@email.kmutnb.ac.th>
2023-07-11 17:20:31 +0700

    updsadsad

---
+++ ex01.js
@@ -0,0 +1,12 @@
+function lessThan100(num1, num2){
+    if(num1 + num2 <= 100)
+    {
+        return true; 
+    }
+    else{return false ;
+    }
+}
+
+console.log(lessThan100(22, 15))
+console.log(lessThan100(83, 34))
+console.log(lessThan100(3, 77))
