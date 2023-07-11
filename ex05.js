8c501ede0877933bb7de8549126d0db53554b700
Tankran007 <s6406021622150@email.kmutnb.ac.th>
2023-07-11 19:25:34 +0700

    updatea

---
+++ ex05.js
@@ -0,0 +1,18 @@
+function leapYear(num1){
+    if(num1%4 == 0)
+    {
+        return true; 
+    }
+    else
+    {return false ;
+    };
+
+}
+console.log(leapYear(1990));
+console.log(leapYear(1924));
+console.log(leapYear(2021));
+console.log(leapYear(2023));
+console.log(leapYear(1992));
+console.log(leapYear(1900));
+
+// คล้ายๆข้อ4 
---
+++ ex06.js
@@ -0,0 +1,7 @@
+function findPerimeter(num1, num2){
+    return (num1*2)+(num2*2);
+
+}
+console.log(findPerimeter(6, 7));
+console.log(findPerimeter(20, 10));
+console.log(findPerimeter(2, 9));
