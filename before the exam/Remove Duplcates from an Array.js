14c2cb320a13209ec450b11761255289e6dcb937
Tankran007 <s6406021622150@email.kmutnb.ac.th>
2023-08-29 22:31:34 +0700

    update

--- before the exam/Remove Duplcates from an Array.js
+++ before the exam/Remove Duplcates from an Array.js
@@ -1,9 +1,7 @@
 function removeDup(arr){
-    return arr.filter((item,
-        index) => arr.indexof(item) === index);
-
+    return arr.filter((item, index) => arr.indexOf(item) === index);
 }
 
-console.log(removeDup(1,0,1,0));
-console.log(removeDup(["The", "big", "cat"]));
-console.log(removeDup(["John", "Taylor", "John"]));
+console.log(removeDup([1, 0, 1, 0])); // Output: [1, 0]
+console.log(removeDup(["The", "big", "cat"])); // Output: ["The", "big", "cat"]
+console.log(removeDup(["John", "Taylor", "John"])); // Output: ["John", "Taylor"]
